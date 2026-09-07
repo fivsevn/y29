502d593f1e6e3c76b8e266e13359c6b1e40503e9
fivsevn <maxd0507@gmail.com>
2026-09-08 04:06:31 +0800

    Square up training watch signal

--- tests/training.test.js
+++ tests/training.test.js
@@ -86,9 +86,11 @@ test('dialogue reveal, compact watch HUD and fixed viewport are built into the r
   assert.match(gameSource,/prefers-reduced-motion: reduce/);
   for(const label of ['PWR','END','MOB','FAT'])assert.match(gameSource,new RegExp(`'${label}'`));
   assert.match(styleSource,/\.stage \{[\s\S]*?width: 100%;[\s\S]*?height: clamp\(/);
-  assert.match(styleSource,/\.sport-watch \{[\s\S]*?right: 13px;[\s\S]*?width: 94px;/);
+  assert.match(styleSource,/\.sport-watch \{[\s\S]*?right: 13px;[\s\S]*?width: 82px;/);
   assert.match(styleSource,/\.watch-top \{[\s\S]*?grid-template-columns: 1fr auto 1fr;/);
   assert.match(gameSource,/watch-time[\s\S]*?watch-date[\s\S]*?watch-status/);
+  assert.match(gameSource,/watch-signal[\s\S]*?<i><\/i><i><\/i><i><\/i>/);
+  assert.match(styleSource,/\.watch-signal i:nth-child\(3\) \{ height: 7px; \}/);
   assert.match(styleSource,/\.watch-strap \{/);
   assert.match(styleSource,/\.watch-legend \{/);
   assert.match(gameSource,/function gameDate\(/);
